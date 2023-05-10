2c1b51e78769b25a81cc7b01c8bc739df57f361a
drowned <fight1999club@gmail.com>
2023-05-10 11:51:32 +0900

    230510

---
+++ part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/Views/RealTimeControl.xaml.cs
@@ -0,0 +1,103 @@
+﻿using MahApps.Metro.Controls;
+using Newtonsoft.Json;
+using SmartHomeMonitoringApp.Logics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace SmartHomeMonitoringApp.Views
+{
+    /// <summary>
+    /// RealTimeControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class RealTimeControl : UserControl
+    {
+        public RealTimeControl()
+        {
+            InitializeComponent();
+
+            LvcLivingTemp.Value = LvcDiningTemp.Value = LvcBedTemp.Value = LvcBathTemp.Value = 0;
+            LvcLivingHumid.Value = LvcDiningHumid.Value = LvcBedHumid.Value = LvcBathHumid.Value = 0;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
+            {
+                // DB 모니터링 실행 뒤 실시간 모니터링으로 넘어왔다면
+                Commons.MQTT_CLIENT.MqttMsgPublishReceived += MQTT_CLIENT_MqttMsgPublishReceived;
+            }
+            else
+            {
+                // DB 모니터링 실행하지 않고 바로 실시간 모니터링 메뉴 클릭 시
+                Commons.MQTT_CLIENT = new MqttClient(Commons.BROKERHOST);
+                Commons.MQTT_CLIENT.MqttMsgPublishReceived += MQTT_CLIENT_MqttMsgPublishReceived;
+                Commons.MQTT_CLIENT.Connect("MONITOR");
+                Commons.MQTT_CLIENT.Subscribe(new string[] { Commons.MQTTTOPIC },
+                    new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+            }
+        }
+
+        private void MQTT_CLIENT_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            var msg = Encoding.UTF8.GetString(e.Message);
+            var currSensor = JsonConvert.DeserializeObject<Dictionary<string, string>>(msg);
+
+            if (currSensor["Home_Id"] == "D101H703")
+            {
+                this.Invoke(() =>
+                {
+                    var dfValue = DateTime.Parse(currSensor["Sensing_DateTime"]).ToString("yyyy-MM-dd HH:mm:ss");
+                    LblSensingDt.Content = $"Sensing DateTime : {dfValue}";
+                });
+                    switch (currSensor["Room_Name"].ToUpper())
+                {
+                    case "LIVING":
+                        this.Invoke(() => {
+                            LvcLivingTemp.Value = Math.Round(Convert.ToDouble(currSensor["Temp"]), 1);
+                            LvcLivingHumid.Value = Math.Round(Convert.ToDouble(currSensor["Humid"]), 1);
+                        });
+                        
+                        break;
+
+                    case "DINING":
+                        this.Invoke(() => {
+                            LvcDiningTemp.Value = Math.Round(Convert.ToDouble(currSensor["Temp"]), 1);
+                            LvcDiningHumid.Value = Math.Round(Convert.ToDouble(currSensor["Humid"]), 1);
+                        });
+                        break;
+
+                    case "BED":
+                        this.Invoke(() => {
+                            LvcBedTemp.Value = Math.Round(Convert.ToDouble(currSensor["Temp"]), 1);
+                            LvcBedHumid.Value = Math.Round(Convert.ToDouble(currSensor["Humid"]), 1);
+                        });
+                        break;
+
+                    case "BATH":
+                        this.Invoke(() => {
+                            LvcBathTemp.Value = Math.Round(Convert.ToDouble(currSensor["Temp"]), 1);
+                            LvcBathHumid.Value = Math.Round(Convert.ToDouble(currSensor["Humid"]), 1);
+                        });
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
